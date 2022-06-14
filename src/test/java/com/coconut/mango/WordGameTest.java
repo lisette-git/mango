9f8123e11fff304cf97b2cf51b486c5db6647e02
lisette-git <95486863+lisette-git@users.noreply.github.com>
2022-06-14 09:52:07 +0200

    wordgame unit tests

---
+++ src/test/java/com/coconut/mango/WordGameTest.java
@@ -0,0 +1,29 @@
+package com.coconut.mango;
+
+
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class WordGameTest {
+    WordGame word = new WordGame();
+
+    @Test
+    public void givenString_getReverseTest() {
+        String in = "iene miene mutte tien pond grutte";
+        String check = "grutte pond tien mutte miene iene ";
+        String out = word.reverseString(in);
+        assertEquals(check,out);
+    }
+
+    @Test
+    public void givenString_getAnagramTest() {
+
+        String in1 = "Debit card";
+        String in2 = "Bad credit";
+        String out = word.anagram(in1, in2);
+        Assert.isTrue(Boolean.parseBoolean(out));
+    }
+}
